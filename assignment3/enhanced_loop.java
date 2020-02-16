4862d07e787648aaf761894a3e5dcad66364e812
sanjok100 <60974681+sanjok100@users.noreply.github.com>
2020-02-16 20:48:48 +0545

    Create enhanced_loop.java

---
+++ assignment3/enhanced_loop.java
@@ -0,0 +1,10 @@
+class Enhancedloop{  
+  public static void main(String args[]){  
+   
+   int arr[]={12,13,14,44};  
+    
+   for(int i:arr){  
+     System.out.println(i);  
+   }  
+ }   
+}  
